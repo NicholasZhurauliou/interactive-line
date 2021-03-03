e235c25479b6971ccf18ce6bfe056e6e5ac36a33
nikolay-zhuravlev <nicholaszhurauliou@gmail.com>
2021-03-03 12:22:03 +0300

    change width line onhover

--- src/App.vue
+++ src/App.vue
@@ -30,7 +30,7 @@ export default {
   methods: {
     calculatePoints(x1, y1, x2, y2) {
       const angle = Math.atan((x2 - x1) / (y2 - y1));
-      const offset = 2.5;
+      const offset = 5;
       const offsetX = offset * Math.cos(angle);
       const offsetY = offset * Math.sin(angle);
       const points = [
@@ -137,7 +137,7 @@ export default {
         .on('click', onClick)
         .on('mouseover', event => {
           this.changeLineStyleColor(event, 0xff7f50);
-          this.changeLineStyleWidth(event, 5, false);
+          this.changeLineStyleWidth(event, 10, false);
         })
         .on('mouseout', event => {
           this.changeLineStyleColor(event, 0xff0000);
